c49d49325737ea2bea1c6f0ced4c6a5acd8f0356
vagrant <vagrant@vagrant.vm>
2019-11-19 15:19:56 +0000

    add new file

---
+++ sample/test/controllers/tweet_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class TweetControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get tweet_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get tweet_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get tweet_new_url
+    assert_response :success
+  end
+
+end
